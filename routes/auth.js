8c73c980bec03e5c357ca75760be3b5fc214a83f
Abdallahman Habyarimana <habdallah005@gmail.com>
2021-02-07 21:23:49 -0500

    Fixing error

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,6 @@ const mongoose = require('mongoose');
 const router = express.Router();
 
 router.post('/', asyncHandler(async(req, res) => {
-  const { error } = validate(req.body); 
-  if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
   if (!user) return res.status(400).send('Invalid email or password.');
--- startup/routes.js
+++ startup/routes.js
@@ -5,5 +5,5 @@ const auth = require('../routes/auth');
 module.exports = function(app) {
   app.use(express.json());
   app.use('/api/users', users);
-  app.use('api/auth', auth)
+  app.use('/api/auth', auth)
 }
